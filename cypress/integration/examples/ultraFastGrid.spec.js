5ab98c99ce30d2383fb80a2d3b03d8406929129b
Leo Laskin <llaskin@gmail.com>
2022-06-23 13:02:00 -0400

    add bad code

--- cypress/integration/examples/ultraFastGrid.spec.js
+++ cypress/integration/examples/ultraFastGrid.spec.js
@@ -5,10 +5,10 @@ describe("AppTest", () => {
         // ⭐️ Note to see visual bugs, run the test using the above URL for the 1st run.
         // but then change the above URL to https://demo.applitools.com/index_v2.html
         // (for the 2nd run)
-//        cy.visit('https://demo.applitools.com');
-//        cy.get(".auth-header").then(($el)=>{
-//         $el.get(0).innerHTML = 'Logout Form';
-//        })
+         cy.visit('https://demo.applitools.com');
+        cy.get(".auth-header").then(($el)=>{
+         $el.get(0).innerHTML = 'Logout Form';
+        })
 
         // Call Open on eyes to initialize a test session
         cy.eyesOpen({
